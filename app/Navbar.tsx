5b5d5ad5cd1c872dd7fcfde15870710b82107486
muhammadtaharauf12 <muhammadtaharauf12@gmail.com>
2026-08-06 20:30:20 +0500

    Style the active link in navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,14 @@
+'use client'
 import Link from "next/link";
 import { FaBug } from "react-icons/fa";
 import React from "react";
+import { usePathname } from "next/navigation";
+import classnames from "classnames"
 
 const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+  
   const links = [
     { label: "dashboard", href: "/" },
     { label: "issues", href: "/issues" },
@@ -15,9 +21,13 @@ const Navbar = () => {
       <ul className="flex gap-6">
         {links.map((link) => (
           <Link
-            key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
             href={link.href}
+            key={link.href}
+            className={classnames({
+              "text-zinc-900":link.href === currentPath,
+              "text-zinc-500":link.href != currentPath,
+              "hover:text-zinc-800 transition-colors":true
+            })}
           >
             {link.label}
           </Link>
