3b8845622e4b1ec22dbe0e6050e0acb2de6c5820
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2026-08-11 03:29:49 +0500

    Handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,40 +1,54 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
-import {useForm,Controller} from "react-hook-form"
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-interface IssueForm{
-  title:string,
-  description:string,
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
 const NewIssuePage = () => {
-  const router = useRouter()
-  const {register,control,handleSubmit} = useForm<IssueForm>();
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
-  
   return (
-    <form 
-    onSubmit={handleSubmit(async (data)=> {
-      await axios.post('/api/issues',data);
-      router.push('/issues')
-    }
-    )}
-    className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title" {...register('title')} />
-      <Controller 
-       name="description"
-       control={control}
-       render={({field})=> <SimpleMDE placeholder="Description" {...field}/>}  //field has same property as register name onChange OnBLur and ref
+    <div className="max-w-xl">
+        {error && <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>}
+    
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post("/api/issues", data);
+          router.push("/issues");
+        } catch (error) {
+          setError("An unexpected error occured")
+          
+        }
+      })}
+      className=" space-y-3"
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )} //field has same property as register name onChange OnBLur and ref
       />
       <Button>Submit New Issue</Button>
     </form>
+    </div>
   );
 };
 
