98a29b9f5aa6543a76c3046b7b524af041bd352f
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2026-08-11 04:02:32 +0500

    Extract the ErrorMessage components

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import {z} from "zod"
+import ErrorMessage from "@/app/components/ErrorMessage";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
@@ -41,7 +42,8 @@ const NewIssuePage = () => {
       className=" space-y-3"
     >
       <TextField.Root placeholder="Title" {...register("title")} />
-      {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+      
+      <ErrorMessage >{errors.title?.message}</ErrorMessage>
       <Controller
         name="description"
         control={control}
@@ -49,7 +51,7 @@ const NewIssuePage = () => {
           <SimpleMDE placeholder="Description" {...field} />
         )} //field has same property as register name onChange OnBLur and ref
       />
-      {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+       <ErrorMessage>{errors.description?.message}</ErrorMessage>
       <Button>Submit New Issue</Button>
     </form>
     </div>
