8389e39c6d34399dc9320fbcb2d9fc2a9895cee7
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2026-08-22 04:12:03 +0500

    Build a style Link compoenet

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,9 @@
 import { prisma } from "@/lib/prisma";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import {  Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay"
 import IssueActions from "./IssueActions";
+import Link from "../components/Link";
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
   await delay(2000);
