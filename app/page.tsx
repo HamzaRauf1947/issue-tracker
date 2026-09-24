cb5bae2ae7998ae15f43826f6415993ccaa5e899
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2026-09-25 04:13:19 +0500

    Build the latestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import Pagination from "./components/Pagination";
-
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ page: string }>;
-}) {
-  const params = await searchParams;
-
-  const currentPage = parseInt(params.page) || 1;
-
+import LatesIssues from "./LatesIssues";
+export default async function Home() {
   return (
+    
     <>
-      <Pagination itemCount={100} pageSize={10} currentPage={currentPage} />
+      <LatesIssues />
+    
     </>
+      
   );
 }
